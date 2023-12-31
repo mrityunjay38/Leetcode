1c539ce62f521b11b87d843f65ebbfbedc600e9d
“Mrityunjay <mrityunjay38@gmail.com>
2023-12-31 19:12:14 +0530

    Easy: #1 Array Two Sum

---
+++ Array/twoSum.js
@@ -0,0 +1,33 @@
+/**
+ * Problem: https://leetcode.com/problems/two-sum/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/**
+ * Solution:
+ */
+
+const twoSum = function (nums, target) {
+  let ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    const remainderIndex = findRemainder(
+      nums.slice(i + 1, nums.length),
+      target - nums[i]
+    );
+    if (remainderIndex > -1) {
+      ans = [i, remainderIndex + i + 1];
+      break;
+    }
+  }
+  return ans;
+};
+
+const findRemainder = (intArray, remainder) => {
+  const index = intArray.indexOf(remainder);
+  return index;
+};
