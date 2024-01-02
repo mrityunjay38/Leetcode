8288a5eccd2938b559f8b06267d34d5ca173ebe6
“Mrityunjay <mrityunjay38@gmail.com>
2024-01-02 11:10:54 +0530

    Easy: #26 Solution 2 using two pointers in O(n)

--- Array_HashMap/26_removeDuplicatesFromSortedArray.js
+++ Array_HashMap/26_removeDuplicatesFromSortedArray.js
@@ -20,3 +20,19 @@ var removeDuplicates = function (nums) {
   }
   return uniqueNums.length;
 };
+
+/**
+ * Solution 2: Two pointer comparison O(n), since array is already sorted in non-decreasing order.
+ */
+
+var removeDuplicates = function (nums) {
+  let j = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] !== nums[i]) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+
+  return j;
+};
