3223d4c9c0ceeb1f869ffa07e01348dac66a656a
Mrityunjay <mrityunjay38@gmail.com>
2024-01-07 21:10:54 +0530

    Medium: #18 4Sum - Similar logic to 3Sum in O(n^3)

---
+++ Array_HashMap/18_fourSum.js
@@ -0,0 +1,46 @@
+/**
+ * Problem: 18 https://leetcode.com/problems/4sum/
+ */
+
+/**
+ * Solution: Similar to 3Sum. Only feasible with O(n^3);
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  const quadruplets = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+      let k = j + 1;
+      let l = nums.length - 1;
+
+      while (k < l) {
+        const sum = nums[i] + nums[j] + nums[k] + nums[l];
+
+        if (sum < target) {
+          k++;
+        } else if (sum > target) {
+          l--;
+        } else {
+          quadruplets.push([nums[i], nums[j], nums[k], nums[l]]);
+          k++;
+          l--;
+          while (k < l && nums[k] === nums[k - 1]) k++;
+          while (k < l && nums[l] === nums[l + 1]) l--;
+        }
+      }
+    }
+  }
+
+  return quadruplets;
+};
