73cb82b8cb0ab9d43802d6eafad442ed44ea7797
Mrityunjay <mrityunjay38@gmail.com>
2024-01-19 00:51:27 +0530

    Medium: #3 Longest substring without repeating characters

---
+++ Array_HashMap/3_longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,37 @@
+/**
+ * Problem: 3 https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+
+/**
+ * Solution: Sliding window & Hashmap T=O(N)
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring = function (s) {
+  let maxSubStr = "";
+  let currSubStr = "";
+
+  let i = 0;
+  let map = new Map();
+
+  while (i < s.length) {
+    if (!map.has(s[i])) {
+      currSubStr += s[i];
+      map.set(s[i], i);
+      i++;
+    } else {
+      maxSubStr = maxSubStr.length < currSubStr.length ? currSubStr : maxSubStr;
+      i = map.get(s[i]) + 1;
+      map.clear();
+      currSubStr = "";
+    }
+  }
+
+  maxSubStr = maxSubStr.length < currSubStr.length ? currSubStr : maxSubStr;
+
+  return s.length < 2 ? s.length : maxSubStr.length;
+};
