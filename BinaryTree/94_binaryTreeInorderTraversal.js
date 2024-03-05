f71a4e73ebd4172157e016d6ad10df398e2507f1
Mrityunjay <mrityunjay38@gmail.com>
2024-03-05 23:15:56 +0530

    #94: Morris Traversal - [Update]

--- BinaryTree/94_binaryTreeInorderTraversal.js
+++ BinaryTree/94_binaryTreeInorderTraversal.js
@@ -3,10 +3,6 @@
  */
 
 /**
- * Solution: T=O(N)
- */
-
-/**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
@@ -18,6 +14,10 @@
 /**
  * @param {TreeNode} root
  * @return {number[]}
+ */
+
+/**
+ * Solution 1: T=O(N), S=O(N) recursion space
  */
 
 var inorderTraversal = function (root) {
@@ -32,4 +32,43 @@ var inorderTraversal = function (root) {
   };
 
   return inorder(root);
+};
+
+/**
+ * Solution 2: T=O(N), S=O(1) if order is printed directly but since a variable is used to store, hence S=O(N)
+ */
+
+/**
+ * Initialize the root as the current node curr.
+ * While curr is not NULL, check if curr has a left child.
+ * If curr does not have a left child, print curr and update it to point to the node on the right of curr.
+ * Else, make curr the right child of the rightmost node in curr's left subtree.
+ * Update curr to this left node.
+ */
+
+var inorderTraversal = function (root) {
+  const inorder = [];
+  let node = root;
+
+  while (node) {
+    if (!node.left) {
+      inorder.push(node.val);
+      node = node.right;
+    } else {
+      let pre = node.left;
+      while (pre.right !== null && pre.right !== node) {
+        pre = pre.right;
+      }
+      if (pre.right === node) {
+        pre.right = null;
+        inorder.push(node.val);
+        node = node.right;
+      } else {
+        pre.right = node;
+        node = node.left;
+      }
+    }
+  }
+
+  return inorder;
 };
