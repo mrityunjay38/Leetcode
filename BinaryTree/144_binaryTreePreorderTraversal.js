e0c66cd3d86fcfed565d1e1f290bc52a5de466ed
Mrityunjay <mrityunjay38@gmail.com>
2024-03-05 23:19:27 +0530

    #144 Morris Traversal - [Update]

--- BinaryTree/144_binaryTreePreorderTraversal.js
+++ BinaryTree/144_binaryTreePreorderTraversal.js
@@ -33,3 +33,40 @@ var preorderTraversal = function (root) {
 
   return preorder(root);
 };
+
+/**
+ * Solution 2: T=O(N), S=O(1) if order is printed directly but since a variable is used to store, hence S=O(N)
+ */
+
+/**
+ * Initialize the root as the current node curr.
+ * While curr is not NULL, check if curr has a left child.
+ * If curr does not have a left child, print curr and update it to point to the node on the right of curr.
+ * Else, make curr the right child of the rightmost node in curr's left subtree.
+ * Print curr and update curr to this left node.
+ */
+
+var preorderTraversal = function (root) {
+  const preorder = [];
+  let node = root;
+  while (node) {
+    if (!node.left) {
+      preorder.push(node.val);
+      node = node.right;
+    } else {
+      let pre = node.left;
+      while (pre.right !== null && pre.right !== node) {
+        pre = pre.right;
+      }
+      if (pre.right === node) {
+        pre.right = null;
+        node = node.right;
+      } else {
+        pre.right = node;
+        preorder.push(node.val);
+        node = node.left;
+      }
+    }
+  }
+  return preorder;
+};
