7b86b2887f4fc57756c1072ff5eacb7d188e6d32
Mrityunjay <mrityunjay38@gmail.com>
2025-12-13 22:07:42 +0530

    Medium: #103 BFS preorder traversal using iterative

--- BinaryTree/103_binaryTreeZigZagLevelOrderTraversal.js
+++ BinaryTree/103_binaryTreeZigZagLevelOrderTraversal.js
@@ -18,6 +18,7 @@
  */
 
 /**
+ * Solution1
  * @param {TreeNode} root
  * @return {number[][]}
  */
@@ -39,4 +40,43 @@ var zigzagLevelOrder = function (root) {
 
   dfs(root, 0);
   return levels;
+};
+
+/**
+ * Solution2: T=O(N)
+ * 1. preorder
+ * 2. maintain leftToRight flip flag
+ * 3. loop while current index in the queue < queue.length
+ * 4. elements before current index are processed, remaining unprocessed
+ * 5. calculate size to allocate for remaining level elements
+ * 6. find position of current node to put at the right index
+ * 7. when leftToRight true, simply put elements at [i=0...size-1]
+ * 8. when leftToRight false, place from right most index, 0,1,2,3 -> 3,2,1,0 i.e [(size-1) - i]
+ */
+
+var zigzagLevelOrder = function (root) {
+  const result = [];
+  if (!root) return result;
+
+  const queue = [root];
+  let index = 0,
+    left2Right = true;
+
+  while (index < queue.length) {
+    const size = queue.length - index;
+    const level = new Array(size);
+
+    for (let i = 0; i < size; i++) {
+      const node = queue[index];
+      index += 1;
+      const pos = left2Right ? i : size - 1 - i;
+      level[pos] = node.val;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+    left2Right = !left2Right;
+  }
+
+  return result;
 };
