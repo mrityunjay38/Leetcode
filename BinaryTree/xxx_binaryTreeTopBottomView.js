11cc85e3568b3ccb3f595b056ad705b0b7e5edfe
Mrityunjay <mrityunjay38@gmail.com>
2025-12-19 20:06:56 +0530

    Hard: GFG Binary tree top view using dfs

--- BinaryTree/xxx_binaryTreeTopBottomView.js
+++ BinaryTree/xxx_binaryTreeTopBottomView.js
@@ -15,7 +15,7 @@ class Node{
 */
 
 /**
- * Solution: BFS Top View, T=O(N) using deque operations
+ * Solution1: BFS Top View, T=O(N) using deque operations
  */
 
 var topView = function (root) {
@@ -51,6 +51,47 @@ var topView = function (root) {
 
   return result;
 };
+
+/**
+ * Solution2: DFS top view, T=O(N)
+ */
+
+class Solution {
+  topView(root) {
+    const colMap = new Map();
+    function dfs(root, col, row) {
+      if (!root) return;
+
+      if (!colMap.has(col)) {
+        colMap.set(col, [[row, root.data]]);
+      } else {
+        const currNodes = colMap.get(col);
+        currNodes.push([row, root.data]);
+        colMap.set(col, currNodes);
+      }
+
+      dfs(root.left, col - 1, row + 1);
+      dfs(root.right, col + 1, row + 1);
+    }
+
+    dfs(root, 0, 0);
+
+    let sortedByCol = [];
+    for (const [key, value] of colMap) {
+      sortedByCol.push([key, value]);
+    }
+
+    sortedByCol = sortedByCol.sort((a, b) => a[0] - b[0]);
+
+    let ans = [];
+    for (const [key, value] of sortedByCol) {
+      const sortedByRow = value.sort((a, b) => a[0] - b[0]);
+      ans.push(sortedByRow[0][1]);
+    }
+
+    return ans;
+  }
+}
 
 /**
  * Solution: BFS Bottom View, T=O(N) using deque operations
