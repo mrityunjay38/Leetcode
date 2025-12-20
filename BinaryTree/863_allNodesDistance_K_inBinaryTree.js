d02a4f164788d46082d94150bef3b7cee4582c76
Mrityunjay <mrityunjay38@gmail.com>
2025-12-21 03:14:39 +0530

    Hard: #863 All nodes at k distance in binary tree using BFS

--- BinaryTree/863_allNodesDistance_K_inBinaryTree.js
+++ BinaryTree/863_allNodesDistance_K_inBinaryTree.js
@@ -18,7 +18,7 @@
  */
 
 /**
- * Solution: T=(O(N) + O(N)) ~ O(N)
+ * Solution1: T=(O(N) + O(N)) ~ O(N)
  * 1. Create graph by adding parent node to each node.
  * 2. Store each node as result for every (k - x = 0) distance as an answer.
  */
@@ -49,4 +49,87 @@ var distanceK = function (root, target, k) {
 
   dfs(target, k);
   return result;
+};
+
+/**
+ * Better/Optimal and interiew friendly
+ * Solution2: BFS, T=O(N)
+ * 1. First mark parents for each node, convert into graph and maintain hashmap to map child --> parent relation
+ * 2. use target as starting point
+ * 3. traverse left, right, upward (parent) for current node and check if already visited to avoid infinte loop since tree is now a graph
+ * 4. everytime we go into next iteration, means we have traversed 1-distance away in left, right, parent directions by pushing them into queue
+ * 5. if distance equals k, break. We found all the k distant nodes left in the queue.
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function (root, target, k) {
+  const hash = new Map();
+  function addParent(root) {
+    const queue = [root];
+    hash.set(root, null);
+
+    while (queue.length) {
+      const node = queue.pop();
+      if (node.left) {
+        node.left.parent = node;
+        hash.set(node.left, node);
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        node.right.parent = node;
+        hash.set(node.right, node);
+        queue.push(node.right);
+      }
+    }
+  }
+
+  const kthNodes = [];
+  const visited = new Map();
+  function kthDistanceNodes(root, target, k) {
+    if (!root) return;
+
+    const queue = [target];
+    visited.set(target, true);
+    let index = 0,
+      distance = 0;
+
+    while (index < queue.length) {
+      const size = queue.length - index;
+
+      if (distance === k) break;
+      distance++;
+
+      for (let i = 0; i < size; i++) {
+        const node = queue[index++];
+
+        if (node.left && !visited.has(node.left)) {
+          queue.push(node.left);
+          visited.set(node.left, true);
+        }
+
+        if (node.right && !visited.has(node.right)) {
+          queue.push(node.right);
+          visited.set(node.right, true);
+        }
+
+        if (hash.get(node) && !visited.has(hash.get(node))) {
+          queue.push(hash.get(node));
+          visited.set(hash.get(node), true);
+        }
+      }
+    }
+
+    while (index < queue.length) {
+      kthNodes.push(queue[index++].val);
+    }
+  }
+
+  addParent(root);
+  kthDistanceNodes(root, target, k);
+  return kthNodes;
 };
